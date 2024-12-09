33afed205d2d61f837910454eba07eb9559335e4
dearguru07 <pangalaguruyadav101@gmail.com>
2024-12-09 20:10:14 +0530

    nmstjs

---
+++ app.js
@@ -0,0 +1,23 @@
+// var a=25
+// function b(){
+//     console.log('Hello world.....')
+// }
+// console.log(a)
+// b()
+// console.log(b)
+
+// let heading=document.createElement("h3")
+// heading.innerHTML="<h3 style={color}>Namastee js </h3>"
+
+
+try {
+  let result = someFunction(); // If `someFunction` is not defined, an error is thrown
+  console.log("Result:", result); // This line won't run if there's an error
+} catch (error) {
+  console.error("An error occurred:", error.message); // Handles the error
+} finally {
+  console.log("This will always run"); // Executes whether there was an error or not
+}
+
+let a;
+console.log(a)
