bb0298cd41b47d474fde9237988611cca88c9432
dearguru07 <pangalaguruyadav101@gmail.com>
2024-12-09 20:18:39 +0530

    k

--- app.js
+++ app.js
@@ -9,15 +9,25 @@
 // let heading=document.createElement("h3")
 // heading.innerHTML="<h3 style={color}>Namastee js </h3>"
 
+// try {
+//   let result = someFunction(); // If `someFunction` is not defined, an error is thrown
+//   console.log("Result:", result); // This line won't run if there's an error
+// } catch (error) {
+//   console.error("An error occurred:", error.message); // Handles the error
+// } finally {
+//   console.log("This will always run"); // Executes whether there was an error or not
+// }
 
-try {
-  let result = someFunction(); // If `someFunction` is not defined, an error is thrown
-  console.log("Result:", result); // This line won't run if there's an error
-} catch (error) {
-  console.error("An error occurred:", error.message); // Handles the error
-} finally {
-  console.log("This will always run"); // Executes whether there was an error or not
+// let a;
+// console.log(a)
+
+
+function x() {
+  var a = 20;
+  function y() {
+    console.log(a);
+  }
+  y();
 }
-
-let a;
-console.log(a)
+let res=x();
+console.log(x)
