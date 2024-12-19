179eda0f1fa44723dbea2314578eea2221eb405e
dearguru07 <pangalaguruyadav101@gmail.com>
2024-12-19 11:49:55 +0530

    pr

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-// a()
-// console.log(b)
-// console.log(a)
-
-// var b=10
-// function a(){
-//   console.log('Hello world...')
+// var a = 1;
+// b();
+// c();
+// console.log(a);
+// function b() {
+//   var a = 10;
+//   console.log(a);
+// }
+// function c() {
+//   var a = 100;
+//   console.log(a);
 // }
 
-var a = 1;
-b();
-c();
-console.log(a);
-function b() {
-  var a = 10;
-  console.log(a);
+
+
+const numbers = [1, 25, 7, 6, 3];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
 }
-function c() {
-  var a = 100;
-  console.log(a);
-}
+
+console.log("Sum of the array:", sum); 
