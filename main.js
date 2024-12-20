617cca1359ef27d8473e6ae55e9ed6686e08f09b
dearguru07 <pangalaguruyadav101@gmail.com>
2024-12-20 13:18:28 +0530

    js

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
-function a(){
-    let b=10
-    c()
-    function c(){
-        console.log(b)
-    }
+// function a(){
+//     let b=10
+//     c()
+//     function c(){
+//         console.log(b)
+//     }
+// }
+// a()
+
+// let a = 20;
+// var b = 10;
+// function x(){
+//     let z=45
+//     console.log(z)
+// }
+// x()
+// console.log(a)
+// console.log(b)
+
+// console.log(a)
+
+// let a =20
+// var b=10
+
+// console.log('bkba')
+// const a=10
+// a=20
+// console.log(a)
+
+
+let b = 10;
+{
+  var a = 100;
+  let b = 20;
+  const c = 30;
+
+  console.log(a);
+  console.log(b);
+  console.log(c);
 }
-a()
+
+// console.log(a);
+console.log(b);
+// console.log(c);
