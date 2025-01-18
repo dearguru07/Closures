aae4a7d50d0936304119f0bfbf3799c6385e0152
dearguru07 <pangalaguruyadav101@gmail.com>
2025-01-18 12:01:00 +0530

    llo

--- app.js
+++ app.js
@@ -199,3 +199,14 @@ const number = [1, 2, 3, 4, 5];
 const sum = number.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
 console.log(sum); // Output: 15
+
+let sol=numbers.reduce((acc,curr)=>
+  acc+curr
+,0)
+console.log(sol)
+
+const number = [1, 2, 3, 4, 5];
+
+const sum = number.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+console.log(sum); // Output: 15
